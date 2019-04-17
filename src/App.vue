b05b129e56a989a8d9cd5bb50a0b659c4208fb18
Abhishek Kovvuri <abhishek.kovvuri@trustedhp.com>
2019-04-17 12:30:12 +0530

    Input Basic Form

--- src/App.vue
+++ src/App.vue
@@ -2,42 +2,32 @@
   <div class="container">
     <div class="row">
       <div class="col-xs-12">
-        <!-- <app-user></app-user> -->
-
         <button @click="selectedComponent='appQuote'">Quote</button>
         <button @click="selectedComponent='appAuthor'">Author</button>
         <button @click="selectedComponent='appNew'">New</button>
-        <p>Selected Component: {{selectedComponent}}</p>
+        <button @click="selectedComponent='appUser'">user</button>
+        <button @click="selectedComponent='basicForm'">Basic Form</button>
+        <p>{{selectedComponent}}</p>
 
         <!-- keep Alive the Component -->
         <keep-alive>
           <!-- Dynamic Component -->
-          <component :is="selectedComponent">
-            <p>Default Content</p>
-          </component>
+          <component :is="selectedComponent"></component>
         </keep-alive>
-        <!-- <app-quote> -->
-        <!-- Passing Content with Slots, and multiple slots-->
-        <!-- <h2 slot="title">{{quoteTitle}}</h2>
-        <p>A WonderFul Quote</p>-->
-        <!-- </app-quote> -->
       </div>
     </div>
   </div>
 </template>
 
 <script>
-// import User from "./components/user/User.vue"
-// import BootstrapVue from "bootstrap-vue"
-// export default {
-//   components: {
-//     appUser: User,
-//   }
-// };
-
+import User from "./components/user/User.vue";
 import Quote from "../src/components/Quote.vue";
 import Author from "../src/components/author/Author.vue";
 import New from "../src/components/author/New.vue";
+//Basic input form binding
+import BasicForm from "../src/components/forms/BasicForm.vue";
+
+
 export default {
   data: function() {
     return {
@@ -48,7 +38,9 @@ export default {
   components: {
     appQuote: Quote,
     appAuthor: Author,
-    appNew: New
+    appNew: New,
+    appUser: User,
+    basicForm: BasicForm
   }
 };
 </script>
